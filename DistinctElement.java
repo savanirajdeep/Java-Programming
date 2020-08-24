88bf32b0c2f5d6403ee8efae52a11766e16e966a
Rajdeepsavani <56822565+savanirajdeep@users.noreply.github.com>
2020-08-24 22:32:12 +0530

    Create DistinctElement.java

---
+++ DistinctElement.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class DistinctElement {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String str=sc.nextLine();
+        System.out.println("String without vowel is : " + vowel(str));
+        System.out.println("Enter the length of array : ");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter elements of array : ");
+        for(int i=0;i<n;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]==a[j]){
+                    int shiftLeft = j;
+                    for (int k=j+1;k<n;k++,shiftLeft++) {
+                        a[shiftLeft] = a[k];
+                    }
+                    n--;
+                    j--;
+                    }
+            }
+        }
+        System.out.print("This is the list without duplicate elements : ");
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+"  ");
+        }
+    }
+    static String vowel(String str) 
+    { 
+         return str.replaceAll("[aeiouAEIOU]", ""); 
+    }
+}
+
