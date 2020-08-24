a57e798750666e4e889fd570b10510c5633e4285
Rajdeepsavani <56822565+savanirajdeep@users.noreply.github.com>
2020-08-24 22:32:57 +0530

    Create FrequencyOfCharacters.java

---
+++ FrequencyOfCharacters.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class FrequencyOfCharacters {
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String str=sc.nextLine();
+        int l=str.length();
+        
+        int[] freq= new int[l];
+        char s[]=str.toCharArray(); 
+        for(int i=0;i<l;i++){
+            freq[i]=1;
+            for(int j=i+1;j<l;j++){
+                if(s[i]==s[j])
+                    freq[i]++;
+            }
+        }
+       
+        System.out.println("Final String : ");
+        int m=freq.length;
+        for(int i=0;i<m;i++){
+            System.out.println(s[i] + " is "+ freq[i]+" times");
+        }
+    }
+}
+
