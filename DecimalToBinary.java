aca6705d5ccd7b74e36d7e244f58c1bac10fd86d
Rajdeepsavani <56822565+savanirajdeep@users.noreply.github.com>
2020-08-25 20:47:43 +0530

    Create DecimalToBinary.java

---
+++ DecimalToBinary.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class BinarytoDecimal {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to convert Binary number : ");
+        int n=sc.nextInt();
+        long bn=0;
+        int r,i=1,step=1;
+
+        while (n!=0)
+        {
+            r=n%2;
+            n/=2;
+            bn+=r*i;
+            i*=10;
+        }
+        System.out.println("Binary number of the given number : "+bn);
+    }    
+}
