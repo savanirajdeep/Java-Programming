6ed96a65484030da2e384d7a2b0d4b1f84beb147
Rajdeepsavani <56822565+savanirajdeep@users.noreply.github.com>
2020-08-25 20:49:59 +0530

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class BinarytoDecimal {
+public class DecimalToBinary {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number to convert Binary number : ");
